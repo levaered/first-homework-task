49d87301edd7061e58864615f08436a91de990e0
levaered <61624689+levaered@users.noreply.github.com>
2023-09-14 23:28:51 +0300

    done✅

---
+++ script.js
@@ -0,0 +1,31 @@
+let copyButton = document.getElementById("copy");
+let result = document.getElementById("result");
+let copiedObj;
+
+copyButton.addEventListener("click", () => {
+    let json = document.getElementById("json").value;
+    json = JSON.parse(json);
+    copiedObj = copyObject(json);
+    alert(copiedObj["arr"]);
+    result.innerHTML = JSON.stringify(copiedObj);
+})
+
+function copyObject(obj) {
+    if (typeof obj !== 'object' || obj === null) {
+        return obj; 
+    }
+
+    if (Array.isArray(obj)) {
+        const newObj = [];
+        for (let i = 0; i < obj.length; i++) {
+            newObj[i] = copyObject(obj[i]);
+        }
+        return newObj;
+    }    
+
+    const newObj = {};
+    for (const key in obj) {
+        newObj[key] = copyObject(obj[key]);
+    }
+    return newObj;
+}
